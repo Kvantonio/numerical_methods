1ca5d9cc823d0a9e2582fd7c1b7a7f5d0eb23dad
kvantonio <kvantonio@i.ua>
2021-05-12 23:53:55 +0300

    flake8

--- nums.py
+++ nums.py
@@ -1,11 +1,7 @@
-import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
-from graphviz import Digraph
-import base64  # noqa: I201
-from io import BytesIO  # noqa: I201
+import base64  # noqa: I201, I100
+import re  # noqa: I201, I100
 
-import re
+from graphviz import Digraph  # noqa: I201, I100
 
 
 class Graph(object):
@@ -20,9 +16,11 @@ class Graph(object):
         return self.bridges
 
     def parseGraph(self, data):
-        temp = [bridge for bridge in re.sub(r'([\r\n\s]+)', '', data).split(',')]
+        temp = [bridge for bridge in re.sub(r'([\r\n\s]+)',
+                                            '', data).split(',')]
         temp = list(set(temp))
-        temp = list(filter(lambda br: len(br) == 2, [br.split('-') for br in temp]))
+        temp = list(filter(lambda br: len(br) == 2,
+                           [br.split('-') for br in temp]))
         for br in temp:
             self.addToGraph(br[0], br[1])
 
@@ -37,7 +35,10 @@ class Graph(object):
             self.graph[x2] = []
 
         self.graph = {x: self.graph[x] for x in sorted(self.graph.keys())}
-        self.bridges = [bridge for bridge in sorted(self.bridges, key=lambda x: (x[0], x[1]))]
+        self.bridges = [bridge for bridge in sorted(
+            self.bridges,
+            key=lambda x: (x[0], x[1])
+        )]
 
     def calcDegree(self):
         return {x: len(self.graph[x]) for x in self.graph.keys()}
@@ -57,7 +58,8 @@ class Graph(object):
         return res
 
     def createIncidenceMatrix(self):
-        res = [[0 for _ in range(sum(list(map(len, self.graph.values()))))] for _ in self.graph.keys()]
+        res = [[0 for _ in range(sum(list(map(len, self.graph.values()))))]
+               for _ in self.graph.keys()]
         for i in range(len(self.graph.keys())):
             for j, bridge in enumerate(self.bridges):
                 if list(self.graph.keys())[i] == bridge[0]:
@@ -69,8 +71,9 @@ class Graph(object):
 
     def incidenceMatrixToTable(self):
         temp = self.createIncidenceMatrix()
-        res = list([[''] + ['q' + str(i + 1) for i, _ in enumerate(temp[0])]]) + \
-              [[item] + temp[i] for i, item in enumerate(self.graph.keys())]
+        res = list([[''] + ['q' + str(i + 1)
+                            for i, _ in enumerate(temp[0])]])
+        res += [[item] + temp[i] for i, item in enumerate(self.graph.keys())]
         return res
 
     def getPreimage(self):
@@ -81,24 +84,13 @@ class Graph(object):
                     res[x].append(items)
         return res
 
-    def drawGraph(self, save=False):
-        #
-        # left = [i[0] for i in self.bridges]
-        # right = [i[1] for i in self.bridges]
-        # df = pd.DataFrame({'from': left, 'to': right})
-        # G = nx.from_pandas_edgelist(df, 'from', 'to', create_using=nx.Graph())
-        # fig = plt.figure(figsize=(8, 8))
-        # nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500)
-
+    def drawGraph(self):
         f = Digraph('graph', filename='static/fsm.gv',
                     node_attr={'color': 'lightblue2',
                                'style': 'filled', 'shape': 'circle'})
         f.attr(rankdir='A', size='1000')
 
         f.edges(self.bridges)
-        # if save:
-        #     plt.savefig('graph.png', facecolor=fig.get_facecolor())
-        # fig.show()
         return f
 
     @staticmethod
@@ -106,72 +98,3 @@ class Graph(object):
         tempfile = fig.pipe(format='png')
         encoded = base64.b64encode(tempfile).decode('utf-8')
         return encoded
-
-
-# graph = Graph()
-#
-# graph.addToGraph('А', 'Б')
-# graph.addToGraph('А', 'В')
-# graph.addToGraph('Б', 'Г')
-# graph.addToGraph('Б', 'Д')
-# graph.addToGraph('В', 'Б')
-# graph.addToGraph('В', 'Г')
-# graph.addToGraph('В', 'Ж')
-# graph.addToGraph('В', 'Е')
-# graph.addToGraph('Г', 'Д')
-# graph.addToGraph('Г', 'Ж')
-# graph.addToGraph('Д', 'Ж')
-# graph.addToGraph('Е', 'Ж')
-# #
-#
-# graph.adjacencyMatrixToTable()
-# graph.incidenceMatrixToTable()
-#
-# # graph.addToGraph(1, 2)
-# # graph.addToGraph(2, 3)
-# # graph.addToGraph(2, 4)
-# # graph.addToGraph(3, 5)
-# # graph.addToGraph(4, 6)
-# # graph.addToGraph(5, 6)
-# # graph.addToGraph(5, 2)
-# # graph.addToGraph(6, 4)
-#
-#
-#
-# print(graph.getGraph())
-#
-# print()
-# print(graph.getBridges())
-# print()
-# degree = graph.calcDegree()
-#
-# print(degree)
-# print()
-# preim = graph.getPreimage()
-#
-# print(preim)
-# print()
-# a = graph.createAdjacencyMatrix()
-#
-# for i in a:
-#     print(i)
-# print()
-# for i in graph.createIncidenceMatrix():
-#     print(i)
-# print()
-#
-# a = graph.drawGraph(save=True)
-#
-# print(graph.graphImgToBytes(a))
-#
-#
-
-
-# gra = Graph()
-# #
-# #
-# #
-# y= 0
-# t = gra.graphImgToBytes(y)
-# print(t)
-# # f.view()
